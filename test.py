1bde1e7a58191c72a0843db1875f23afc0eda811
v.markov <21138532110q@gmail.com>
2020-04-19 16:48:39 +0500

    small fixes for launch test

--- test.py
+++ test.py
@@ -43,26 +43,26 @@ def str2bool(v):
     return v.lower() in ("yes", "y", "true", "t", "1")
 
 parser = argparse.ArgumentParser(description='CRAFT Text Detection')
-parser.add_argument('--trained_model', default='weights/craft_mlt_25k.pth', type=str, help='pretrained model')
+parser.add_argument('--trained_model', default='./pretrain/91.pth', type=str, help='pretrained model')
 parser.add_argument('--text_threshold', default=0.7, type=float, help='text confidence threshold')
 parser.add_argument('--low_text', default=0.4, type=float, help='text low-bound score')
 parser.add_argument('--link_threshold', default=0.4, type=float, help='link confidence threshold')
-parser.add_argument('--cuda', default=True, type=str2bool, help='Use cuda to train model')
+parser.add_argument('--cuda', default=False, type=str2bool, help='Use cuda to train model')
 parser.add_argument('--canvas_size', default=2240, type=int, help='image size for inference')
 parser.add_argument('--mag_ratio', default=2, type=float, help='image magnification ratio')
 parser.add_argument('--poly', default=False, action='store_true', help='enable polygon type')
 parser.add_argument('--show_time', default=False, action='store_true', help='show processing time')
-parser.add_argument('--test_folder', default='/data/', type=str, help='folder path to input images')
+parser.add_argument('--test_folder', default='./data/', type=str, help='folder path to input images')
 
 args = parser.parse_args()
 
-
 """ For test images in a folder """
-image_list, _, _ = file_utils.get_files('/data/CRAFT-pytorch/test')
+image_list, _, _ = file_utils.get_files('./data/')
 
-result_folder = '/data/CRAFT-pytorch/result/'
+result_folder = './data/result/'
 if not os.path.isdir(result_folder):
     os.mkdir(result_folder)
+
 
 def test_net(net, image, text_threshold, link_threshold, low_text, cuda, poly):
     t0 = time.time()
@@ -109,7 +109,6 @@ def test_net(net, image, text_threshold, link_threshold, low_text, cuda, poly):
     return boxes, polys, ret_score_text
 
 
-
 def test(modelpara):
     # load net
     net = CRAFT()     # initialize
@@ -127,19 +126,25 @@ def test(modelpara):
 
     net.eval()
 
+    print('Start eval')
     t = time.time()
 
     # load data
     for k, image_path in enumerate(image_list):
-        print("Test image {:d}/{:d}: {:s}".format(k+1, len(image_list), image_path), end='\r')
         image = imgproc.loadImage(image_path)
+        print(f'Start {image_path}')
 
         bboxes, polys, score_text = test_net(net, image, args.text_threshold, args.link_threshold, args.low_text, args.cuda, args.poly)
         # save score text
+        print(f'end {image_path}')
         filename, file_ext = os.path.splitext(os.path.basename(image_path))
-        mask_file = result_folder + "/res_" + filename + '_mask.jpg'
-        #cv2.imwrite(mask_file, score_text)
+        mask_file = result_folder + "res_" + filename + '_mask.jpg'
+        cv2.imwrite(mask_file, score_text)
 
         file_utils.saveResult(image_path, image[:,:,::-1], polys, dirname=result_folder)
 
     print("elapsed time : {}s".format(time.time() - t))
+
+
+if __name__ == '__main__':
+    test(args.trained_model)
